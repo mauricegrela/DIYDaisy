9848a7b9235c130eadddae4f74fcc95d21cbb6e5
Terry <terry@tdee.ca>
2018-03-20 10:40:42 -0700

    Taking out Lorem Ipsum for CBC

--- js/states/stateParentsCorner.js
+++ js/states/stateParentsCorner.js
@@ -21,9 +21,9 @@ var StateParentsCorner = {
         var PDFButtonsPos_y = game.height/2; 
         var PDFButtonScale = 0.5;
         
-        var Credits = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-        var TermsAndConditions = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-        var privacystatement = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        var Credits = "Credits";
+        var TermsAndConditions = "Terms And Conditions";
+        var privacystatement = "Privacy";
         
         
                 
