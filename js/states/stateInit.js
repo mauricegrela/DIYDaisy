451ceb94858531985b04ddc2740e91155bcd43c0
Maurice Grela <maurice.grela@gmail.com>
2018-05-31 13:07:16 -0700

    Committing implemented critiques

--- js/states/stateInit.js
+++ js/states/stateInit.js
@@ -27,7 +27,49 @@ var StateInit = {
             }
 
     }
-    , create: function () {        
+    
+    ,checkAudioContext: function()
+    {
+            //work out when the audio context has stopped
+
+            if(this.game.sound.context.currentTime-this.last_context_time===0){
+
+                //close out the existing context and create a new one
+                //you will also need new gain nodes if you are using them
+
+                this.game.sound.context.close();  
+                this.game.sound.context=new AudioContext(); 
+                this.game.sound.masterGain= this.game.sound.context.createGain(); 
+                this.game.sound.masterGain.gain.volume=this.volume;
+                this.game.sound.masterGain.connect(this.game.sound.context.destination);
+
+                //now go through every sound and connect them to the new context
+                //creating gain nodes as we go.
+                for(var key in this.tracks){
+
+                    var snd=this.tracks[key].snd;
+                    snd.context=this.game.sound.context;
+                    snd.masterGainNode = this.game.sound.masterGain;
+                    snd.gainNode=this.game.sound.context.createGain();          
+                    snd.gainNode.gain.value = snd.volume * this.volume;
+                    snd.gainNode.connect(snd.masterGainNode);
+                }       
+
+            }else{
+            //update out time variable
+            this.last_context_time=ctx.currentTime;
+            }
+    }
+    
+    , create: function () {    
+        
+        
+        ///This is run using a timer event every second
+
+this.game.time.events.loop(1000, this.checkAudioContext, this);
+
+
+        
       this.game.stage.backgroundColor = "#9dbf58";
         game.state.start("StateLoad");
     }
